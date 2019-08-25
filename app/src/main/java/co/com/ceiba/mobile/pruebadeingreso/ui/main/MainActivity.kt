76b50ce6f5330e383d929f8ba7bcf9543698b013
Felipe Arcos <pipearcos221@gmail.com>
2019-08-25 01:26:16 -0500

    se creo extension para lifecycle

--- app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/ui/main/MainActivity.kt
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/ui/main/MainActivity.kt
@@ -6,15 +6,18 @@ import android.os.Bundle
 
 import co.com.ceiba.mobile.pruebadeingreso.R
 import co.com.ceiba.mobile.pruebadeingreso.databinding.ActivityMainBinding
+import co.com.ceiba.mobile.pruebadeingreso.ui.adapters.UserAdapter
 
 
 class MainActivity : Activity() {
 
     private lateinit var binding : ActivityMainBinding
+    private val adapter = UserAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.recyclerViewSearchResults.adapter = adapter
     }
 
 
---
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/util/RxExts.kt
@@ -0,0 +1,27 @@
+package co.com.ceiba.mobile.pruebadeingreso.util
+
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.LifecycleObserver
+import android.arch.lifecycle.LifecycleOwner
+import android.arch.lifecycle.OnLifecycleEvent
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+class LifeDisposable(owner: LifecycleOwner) : LifecycleObserver {
+
+    init {
+        owner.lifecycle.addObserver(this)
+    }
+
+    private val dis : CompositeDisposable = CompositeDisposable()
+
+    infix fun add(disposable: Disposable){
+        dis.add(disposable)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun clearDisposable(){
+        dis.clear()
+    }
+
+}
